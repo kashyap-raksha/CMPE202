7eeb92948c4c412b8f1a1bf23514b71b8a764d7a
kashyap-raksha <kraksha39@gmail.com>
2017-03-11 00:58:24 -0800

    Initial commit

--- src/umlParser/MainParser.java
+++ src/umlParser/MainParser.java
@@ -1,14 +1,13 @@
-package umlParser;
+package src.umlParser;
 
 import java.io.IOException;
-import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class MainParser {
 
-	public static void main(String[] args) throws IOException, AccessDeniedException  {
+	public static void main(String[] args) throws IOException  {
 		//FileInputStream in = new FileInputStream("C:/Windows/uml-parser-test-1");
 		
 		Stream<String> lines = Files.lines(Paths.get("C:/Windows/uml-parser-test-1"));
