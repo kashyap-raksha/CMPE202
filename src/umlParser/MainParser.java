cb8cfa769e48643912788f6f31c79ec7a8fec376
kashyap-raksha <kraksha39@gmail.com>
2017-04-07 23:41:13 -0700

    Latest commit

--- src/umlParser/MainParser.java
+++ src/umlParser/MainParser.java
@@ -1,29 +1,31 @@
 package src.umlParser;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class MainParser {
+	
+	private static File src_files = null;
+	private static File[] listOfFiles = null;
 
 	public static void main(String[] args) throws IOException  {
 		//FileInputStream in = new FileInputStream("C:/Windows/uml-parser-test-1");
 		
 		//Stream<String> lines = Files.lines(Paths.get("C:/Windows/uml-parser-test-1"));
+		if (args.length == 2){
+			String src = args[0];
+			String outputfile = args[1];
+			src_files = new File(src);		
+		}
+		else
+		{
+			System.out.println("Provide all required arguments.");
+		}
 		
-		String src = args[0];
-		String outputfile = args[1];
 		
 		FileInputStream in = new FileInputStream(src);
 		System.out.println(in);
-		if (src.isEmpty()){
-			System.out.println("Please enter a source file name");
-		}
-		else{
-			//CompilationUnit parse = JavaParser.parse(src);
-			System.out.println("A source file exists");
-		}
-		}
-
+		
 	}
-
 }
