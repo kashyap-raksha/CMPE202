40e793657f66465a001be8fb4a635cf017eebcb6
kashyap-raksha <kraksha39@gmail.com>
2017-04-15 02:01:09 -0700

    Initial commit

--- src/umlParser/javaParser.java
+++ src/umlParser/javaParser.java
@@ -1,7 +1,11 @@
 package src.umlParser;
 
+import java.io.File;
+
 public class javaParser {
 	
-	
+	public void startParsing(File file){
+		//read the file for different keywords in the files.
+	}
 
 }
