a8cfab6e6ecf48c4537863610ebe061f3bde2fb9
kashyap-raksha <kraksha39@gmail.com>
2017-04-15 20:26:49 -0700

    Adding constructor

--- src/src/umlParser/javaParser.java
+++ src/src/umlParser/javaParser.java
@@ -1,11 +1,29 @@
 package src.umlParser;
 
 import java.io.File;
+import java.util.ArrayList;
+
+import japa.parser.ast.CompilationUnit;
 
 public class javaParser {
 	
+	final String inputPath;
+    final String outPath;
+    String yumlCode;
+	ArrayList<CompilationUnit> array;
+	
+	public javaParser(String args, String args2){
+		//constructor
+		inputPath = args;
+		outPath = args+"\\" + args2 + ".png";
+		System.out.println(inputPath);
+		System.out.println(outPath);
+	}
+	
 	public void startParsing(File file){
 		//read the file for different keywords in the files.
+		
+		
 	}
 
 }
