41197ecf0ce01bafcd698895dc007d5c3f0a828b
kashyap-raksha <kraksha39@gmail.com>
2017-04-15 20:29:22 -0700

    Adding all method stubs to the class

--- src/src/umlParser/javaParser.java
+++ src/src/umlParser/javaParser.java
@@ -23,6 +23,39 @@ public class javaParser {
 	public void startParsing(File file){
 		//read the file for different keywords in the files.
 		
+		array = get_Compilation_Array(inputPath);
+        buildMap(array);
+        for (CompilationUnit cu : array){
+            yumlCode += parser(cu);
+        }
+        yumlCode += parsed_code();
+        yumlCode = yuml_UniqueLines(yumlCode);
+        System.out.println("Unique Code: " + yumlCode);
+        //class for generate diagram.
+    }
+
+	private ArrayList<CompilationUnit> get_Compilation_Array(String inputPath2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String parser(CompilationUnit cu) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String yuml_UniqueLines(String yumlCode2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private String parsed_code() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private void buildMap(ArrayList<CompilationUnit> array2) {
+		// TODO Auto-generated method stub
 		
 	}
 
